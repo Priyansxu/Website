4e60ab7ffad57254d3beae799ed18c37ee14e1ad
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-24 22:16:32 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,8 +1,11 @@
-export default function header() {
+const header = () => {
   return (
-    <div>
-      <h1>Welcome to AnyComponent</h1>
-      <p>This is a sample component.</p>
+    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-xl lg:text-2xl text-black mb-20">
+      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-400 bg-gradient-to-b from-[#f5f5f5] to-[#e5e5e5] pb-2 pt-4 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2">
+        Priyanshu
+      </p>
     </div>
   );
-}
+};
+
+export default header;
