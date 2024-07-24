e3a3ae5684bbd97db700af69299320e5e570d32d
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-24 17:27:11 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css';
+import { Inter } from 'next/font/google'
+import '../styles/globals.css'
 
-function Web({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const fontHeading = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
+function Website({ Component, pageProps }) {
+  return (
+    <div 
+      className={`antialiased ${fontHeading.variable} ${fontBody.variable}`}
+    >
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default Web;
+export default Website;
