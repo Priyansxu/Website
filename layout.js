92741b2997c161589cc8ab57d34220fee72c54ea
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-24 17:52:20 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,24 +1,11 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const fontHeading = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
-})
-
-const fontBody = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
-})
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${fontHeading.variable} ${fontBody.variable}`}>
+    <html lang="en">
       <body className="antialiased">
         {children}
       </body>
     </html>
-  )
+  );
 }
