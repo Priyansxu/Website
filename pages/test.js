c64b4c23726c97130c1f8b27477e020538ce751f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 05:47:57 +0530

    Update pages

---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import ElementX from "../public/element-x.png";
+export default function Test() {
+return (
+<div className="item-centre flex blur-md">
+<Image 
+  src={ElementX}
+  alt="x"
+  height={150}
+  width={150}
+/>
+ );
+}
