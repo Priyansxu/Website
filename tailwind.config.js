eb7d4e61b4a9ddfa91ba656e11da2bdac33d24be
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 12:47:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spaceMono: ['SpaceMono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
