4b62d33333605bab989a4920d44d3b221b32b4a1
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 05:56:54 +0530

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import ElementX from "../public/element-x.png";
 export default function Test() {
 return (
-<div className="flex justify-center items-center relative mt-10 blur-md">
+<div className="flex justify-center items-center relative mt-10 blur-lg">
 <Image 
   src={ElementX}
   alt="x"
