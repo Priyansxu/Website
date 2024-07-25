ff77af023c11251e61fff409b860a67a24170644
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 09:03:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Page() {
             />
           </div>
         </section>
-        <section className="mt-6 mx-4">
+        <section className="mt-6 mx-4 md:mx-10">
 <div className="rounded bg-gradient-to-br from-[#f5f5f5] to-[#d5d5d5] flex items-center justify-center">
             <div className="relative w-1/2 h-48 md:h-full">
               <Image
