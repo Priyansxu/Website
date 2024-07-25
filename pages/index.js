e4504730550ca4244d7c41650f07f0a1bde09ee1
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 08:33:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Page() {
             ))}
           </motion.p>
           <div className="flex justify-center items-center relative mt-6">
-            <div className="absolute inset-0 backdrop-blur-lg py-8 z-20"></div>
+            <div className="absolute inset-0 backdrop-blur-md p-6 z-20"></div>
             <Image
               src={ElementX}
               alt="Element"
@@ -66,21 +66,19 @@ export default function Page() {
           </div>
         </section>
         <section className="mt-6 mx-6">
-          <div className="rounded bg-gradient-to-br from-[#f5f5f5] to-[#d5d5d5] flex items-center justify-center p-8">
-            <div className="flex w-full max-w-4xl h-96">
-              <div className="relative w-1/2 h-full">
-                <Image
-                  src={Helmet}
-                  alt="Helmet"
-                  layout="fill"
-                  objectFit="contain"
-                />
-              </div>
-              <div className="w-1/2 flex items-center justify-center">
-                <p className="text-sm text-gray-700 mx-6">
-                  This is a paragraph text that is displayed on the right side of the image. You can customize this text to include any content you like.
-                </p>
-              </div>
+          <div className="rounded bg-gradient-to-br from-[#f5f5f5] to-[#d5d5d5] flex flex-col lg:flex-row items-center justify-center p-8">
+            <div className="relative w-full lg:w-1/2 h-64 lg:h-full">
+              <Image
+                src={Helmet}
+                alt="Helmet"
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <div className="w-full lg:w-1/2 flex items-center justify-center mt-6 lg:mt-0">
+              <p className="text-sm text-gray-700 mx-4">
+                This is a paragraph text that is displayed on the right side of the image. You can customize this text to include any content you like.
+              </p>
             </div>
           </div>
         </section>
