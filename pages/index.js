1290fe1cd9ab5883b039f8bb03396ccd4b44ccb3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 06:54:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Page() {
             ))}
           </motion.p>
           <div className="flex justify-center items-center relative mt-10">
-     <div className="absolute inset-0 opacity-50 backdrop-blur-md z-20">
+     <div className="absolute inset-0 opacity-50 backdrop-blur-sm z-20">
     </div>
             <Image
               src={ElementX}
