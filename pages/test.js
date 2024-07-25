f86a7aa08b2624ce2e08bc294c20e66179fa4e3b
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-25 06:15:40 +0530

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,13 +2,15 @@ import Image from "next/image";
 import ElementX from "../public/element-x.png";
 export default function Test() {
 return (
-<div className="flex justify-center items-center relative mt-10 blur-lg">
-<Image 
-  src={ElementX}
-  alt="x"
-  height={150}
-  width={150}
-/>
+<div className="relative flex justify-center items-center mt-10">
+  <div className="absolute inset-0 bg-white bg-opacity-50 backdrop-blur-md"></div>
+  <Image 
+    src={ElementX}
+    alt="x"
+    height={150}
+    width={150}
+    className="relative z-10"
+  />
 </div>
  );
 }
