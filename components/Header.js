d53391b1dca5c6e90e01e164f9923e129aec9ea2
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-27 23:23:22 +0530

    Update components

---
+++ components/Header.js
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Fluid from "../public/fluid.png";
+import { Paragraph, Heading } from "../motions/animationVariants";
+
+const paragraphText =
+  "Welcome to the futuristic profile of Priyanshu. Here, you will find information about my projects, skills, and contact details.";
+
+const Header = () => (
+  <section className="p-8 text-center">
+    <motion.h1
+      className="text-4xl font-bold text-black bg-clip-text pb-3"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+    >
+      Priyanshu
+    </motion.h1>
+    <motion.p
+      className="mt-4 text-lg font-spaceMono text-gray-600 backdrop-blur-md z-30"
+      initial="hidden"
+      animate="visible"
+      variants={Paragraph}
+    >
+      {paragraphText.split("").map((char, index) => (
+        <motion.span key={index} variants={Heading}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.p>
+    <div className="flex justify-center items-center relative mt-6">
+      <div className="absolute inset-0 backdrop-blur-md p-6 z-30"></div>
+      <Image
+        src={Fluid}
+        alt="Fluid"
+        height={150}
+        width={600}
+        className="relative opacity-70 z-20 w-full max-w-md"
+      />
+    </div>
+  </section>
+);
+
+export default Header;
