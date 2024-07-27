22912a7375be595e57a68611f09cf7b59c493cb1
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-27 11:41:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ export default function HomePage() {
         </motion.p>
 <section ref={secondSectionRef} className="flex my-4">
   <motion.div
-    className="mx-4 md:mx-0 w-150 rounded-md md:rounded-none md:w-full h-85 md:h-90 flex bg-gradient-to-b from-[a5a5a5] via-neutral-300 to-[#cdcdcd] shadow-xl overflow-hidden relative"
+    className="mx-4 md:mx-0 w-150 rounded-md md:rounded-none md:w-full h-85 md:h-90 flex bg-gradient-to-b from-[a5a5a5] via-neutral-300 to-[#cdcdcd] shadow-xl overflow-hidden"
     initial="hidden"
     animate={isInView ? "visible" : "hidden"}
     variants={Div}
@@ -128,7 +128,7 @@ export default function HomePage() {
     </div>
 
     <motion.div
-      className="flex-none w-1/3 z-30 backdrop-blur-md"
+      className="flex-none w-1/3 z-30 backdrop-blur-md relative"
       initial="hidden"
       animate={isInView ? "visible" : "hidden"}
       variants={Img}
