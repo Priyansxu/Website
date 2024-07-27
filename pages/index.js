6aba771518753c3bc05b979ba05399ff9cf11cc0
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-27 11:30:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Fluid from "../public/fluid.png";
 import Helmet from "../public/helmet.png";
 import Particles from "../components/Particles";
+import Element from "../public/elementx.png";
 import { Paragraph, Heading, Letter, Img, Div } from "../motions/animationVariants";
 
 const paragraphText =
@@ -104,6 +105,42 @@ export default function HomePage() {
             </motion.span>
           ))}
         </motion.p>
+<section ref={secondSectionRef} className="flex my-4">
+          <motion.div
+            className="mx-4 md:mx-0 w-150 rounded-md md:rounded-none md:w-full h-85 md:h-90 flex bg-gradient-to-b from-[a5a5a5] via-neutral-300 to-[#cdcdcd] shadow-xl overflow-hidden backdrop-blur-md z-30 relative"
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            variants={Div}
+          >
+            <div className="flex-grow pr-6 text-xs md:text-2xl flex items-center justify-center text-left">
+              <motion.p
+                className="py-4 mx-4"
+                initial="hidden"
+                animate={isInView ? "visible" : "hidden"}
+                variants={Paragraph}
+              >
+                {secondSectionText.split("").map((char, index) => (
+                  <motion.span key={index} variants={Letter}>
+                    {char}
+                  </motion.span>
+                ))}
+              </motion.p>
+            </div>
+
+            <motion.div
+              className="flex-none w-1/3"
+              initial="hidden"
+              animate={isInView ? "visible" : "hidden"}
+              variants={Img}
+            >
+              <Image
+                src={Element}
+                alt="X"
+                className="object-contain object-cover h-full w-full opacity-60 absolute inset-0 z-20"
+              />
+            </motion.div>
+          </motion.div>
+        </section>
       </main>
     </div>
   );
