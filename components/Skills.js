734eee28c3f2f453afaf80e71956695f45eae2b3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-29 10:50:19 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -1,6 +1,6 @@
 export default function Skills() {
 return (
 <h1 className="font-monaSans ml-4">Skills</h1>
-<p className="text-sm font-mono m-3">Here are some languages and frameworks I've learnt in a year.</p>
+<h4 className="text-sm font-mono m-3">Here are some languages and frameworks I've learnt in a year.</h4>
   );
 }
