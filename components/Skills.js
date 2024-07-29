04d8bd4f2c40029103443cf0cd7f51b77b966ec5
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-29 10:44:18 +0530

    Update components

---
+++ components/Skills.js
@@ -0,0 +1,6 @@
+export default function Profile() {
+return (
+<h1 className="font-monaSans ml-4">Skills</h1>
+<p className="text-sm font-mono m-3">Here are some languages and frameworks I've learnt in a year.</p>
+  );
+}
