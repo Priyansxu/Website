f0d72459bce78434919643c20159913b37e7948c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-07-30 21:01:35 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -8,23 +8,29 @@ export default function Skills() {
       <h5 className="text-sm text-left font-monaSansM mt-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
-      <div className="flex flex-wrap mt-6">
-        {languages.map((language, index) => (
-          <div
-            key={index}
-            className="rounded-md border border-gray-200 p-2 m-2 text-center"
-          >
-            {language}
-          </div>
-        ))}
-        {frameworks.map((framework, index) => (
-          <div
-            key={index}
-            className="rounded-md border border-gray-200 p-2 m-2 text-center"
-          >
-            {framework}
-          </div>
-        ))}
+      <div className="mt-6">
+        <h2 className="text-xl text-black font-monaSansM">Languages</h2>
+        <div className="flex flex-wrap mt-2">
+          {languages.map((language, index) => (
+            <div
+              key={index}
+              className="rounded-md border border-gray-400 bg-gradient-to-br from-[#a5a5a5] via-[#f5f5f5] to-[#dcdcdc] p-1 m-1 text-center text-sm"
+            >
+              {language}
+            </div>
+          ))}
+        </div>
+        <h2 className="text-xl text-black font-monaSansM mt-4">Frameworks</h2>
+        <div className="flex flex-wrap mt-2">
+          {frameworks.map((framework, index) => (
+            <div
+              key={index}
+              className="rounded-md border bg-gradient-to-br from-[#a5a5a5] via-[#f5f5f5] to-[#dcdcdc] border-gray-400 p-1 m-1 text-center text-sm"
+            >
+              {framework}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
