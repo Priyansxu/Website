16632e131b46615633964ebc6e3b0a88ed400235
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-01 14:10:21 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -11,29 +11,29 @@ export default function Skills() {
 
   return (
     <div className="pt-10 m-4">
-      <h1 className="text-4xl text-black font-monaSans">Skills</h1>
-      <h5 className="text-sm text-left font-monaSansM mt-4">
+      <h1 className="text-4xl text-[#A3A3A3] font-monaSans">Skills</h1>
+      <h5 className="text-sm text-left text-[#A3A3A3] font-monaSansM mt-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
       <div className="mt-6">
-        <h2 className="text-xl text-black font-monaSansM">Languages</h2>
+        <h2 className="text-xl text-[#A3A3A3] font-monaSansM">Languages</h2>
         <div className="flex flex-wrap mt-2">
           {languages.map((language, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-600 bg-gradient-to-b from-gray-800 to-gray-900 p-2 m-1 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-1 text-center text-sm text-[#A3A3A3]"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[language]}`}></div>
               {language}
             </div>
           ))}
         </div>
-        <h2 className="text-xl text-black font-monaSansM mt-4">Frameworks</h2>
+        <h2 className="text-xl text-[#A3A3A3] font-monaSansM mt-4">Frameworks</h2>
         <div className="flex flex-wrap mt-2">
           {frameworks.map((framework, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-600 bg-gradient-to-b from-gray-800 to-gray-900 p-2 m-1 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-1 text-center text-sm text-[#A3A3A3]"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[framework]}`}></div>
               {framework}
