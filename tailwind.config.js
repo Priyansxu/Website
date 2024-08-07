c753524dde3d608ce93a381146a82ca753db8e3c
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 23:15:58 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,13 @@ module.exports = {
       keyframes: {
         scroll: {
           '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(-50%)' },
         },
       },
       animation: {
-        scroll: 'scroll 10s linear infinite',
+        scroll: 'scroll 20s linear infinite',
       },
     },
   },
   plugins: [],
-}
+};
