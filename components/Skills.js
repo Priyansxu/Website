0c31815fa48a34c42f5df3b8df336e8477709ea2
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 19:08:14 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -15,14 +15,13 @@ export default function Skills() {
       <h5 className="text-sm text-left font-monaSansM m-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
-   <div className="mx-4">
-      <div className="py-3 m-4 border border-[#b6b6b6] bg-gradient-to-br via-[#71747B] to-[#4A4B4F] rounded-lg">
+<div className="py-3 m-4 border border-[#b6b6b6] bg-gradient-to-br via-[#71747B] to-[#4A4B4F] rounded-lg">
         <h2 className="text-xl text-gray-700 font-monaSansM m-2">Languages</h2>
         <div className="flex flex-wrap mt-2">
           {languages.map((language, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 opacity-60 p-2 m-2 text-center text-sm text-white"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[language]}`}></div>
               {language}
@@ -34,14 +33,13 @@ export default function Skills() {
           {frameworks.map((framework, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-2 text-center text-sm text-white"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[framework]}`}></div>
               {framework}
             </div>
           ))}
         </div>
-      </div>
      </div>
     </div>
   );
