cb488a074f1e4fc3040350677a43e985433e555f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 20:05:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         monaSans: ['MonaSans', 'sans-serif'],
         monaSansM: ['MonaSansM', 'sans-serif'],
       },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 10s linear infinite',
+      },
     },
   },
   plugins: [],
