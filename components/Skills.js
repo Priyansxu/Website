3664d87aa90351032072b90ef33477151e0f49d2
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 20:07:15 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import { FaPython, FaJs, FaReact } from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
+
 export default function Skills() {
-  const languages = ["Python", "JavaScript"];
-  const frameworks = ["Next.js", "Tailwind CSS"];
+
+const icons = [
+  { name: 'Python', icon: <FaPython className="text-6xl" /> },
+  { name: 'JavaScript', icon: <FaJs className="text-6xl" /> },
+  { name: 'Tailwind CSS', icon: <SiTailwindcss className="text-6xl" /> },
+  { name: 'Next.js', icon: <SiNextdotjs className="text-6xl" /> },
+  { name: 'React', icon: <FaReact className="text-6xl" /> },
+];
 
   return (
     <div className="py-3 m-4 bg-[#a5a5a5]/60 rounded-lg">
@@ -9,29 +19,21 @@ export default function Skills() {
         Here are some languages and frameworks I've learnt in a year.
       </h5>
       <div className="py-3 m-3 border border-[#b6b6b6] bg-gradient-to-br from-[#4A4B4F]/50 via-[#71747B] to-[#4A4B4F] opacity-90 rounded-lg">
-  <h2 className="text-xl text-gray-300 font-monaSansM m-2">Languages</h2>
-  <div className="flex flex-wrap mt-2">
-    {languages.map((language, index) => (
-      <div
-        key={index}
-        className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-1 m-1 text-center text-sm text-white"
-      >
-        {language}
+        <div className="relative overflow-hidden bg-gray-200 py-4">
+          <div className="flex animate-scroll">
+            {icons.map((icon, index) => (
+              <div key={index} className="flex-none mx-4">
+                {icon.icon}
+              </div>
+            ))}
+            {icons.map((icon, index) => (
+              <div key={index} className="flex-none mx-4">
+                {icon.icon}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    ))}
-  </div>
-  <h2 className="text-xl text-gray-300 font-monaSansM m-2 mt-4">Frameworks</h2>
-  <div className="flex flex-wrap mt-2">
-    {frameworks.map((framework, index) => (
-      <div
-        key={index}
-        className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-1 m-1 text-center text-sm text-white"
-      >
-        {framework}
-      </div>
-    ))}
-      </div>
-     </div>
     </div>
   );
 }
