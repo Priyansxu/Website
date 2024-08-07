6db34bca6bfd842f2958a78c34297baa0927e035
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 23:15:06 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -3,14 +3,13 @@ import { FaPython, FaJs, FaReact } from 'react-icons/fa';
 import { RiTailwindCssFill, RiNextjsFill } from 'react-icons/ri';
 
 export default function Skills() {
-
-const icons = [
-  { name: 'Python', icon: <FaPython className="text-6xl" /> },
-  { name: 'JavaScript', icon: <FaJs className="text-6xl" /> },
-  { name: 'Tailwind CSS', icon: <RiTailwindCssFill className="text-6xl" /> },
-  { name: 'Next.js', icon: <RiNextjsFill className="text-6xl" /> },
-  { name: 'React', icon: <FaReact className="text-6xl" /> },
-];
+  const icons = [
+    { name: 'Python', icon: <FaPython className="text-6xl" /> },
+    { name: 'JavaScript', icon: <FaJs className="text-6xl" /> },
+    { name: 'Tailwind CSS', icon: <RiTailwindCssFill className="text-6xl" /> },
+    { name: 'Next.js', icon: <RiNextjsFill className="text-6xl" /> },
+    { name: 'React', icon: <FaReact className="text-6xl" /> },
+  ];
 
   return (
     <div className="py-3 m-4 bg-[#a5a5a5]/60 rounded-lg">
@@ -18,9 +17,9 @@ const icons = [
       <h5 className="text-sm text-left font-monaSansM m-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
-      <div className=" m-3">
+      <div className="m-3">
         <div className="relative overflow-hidden bg-[#b6b6b6] rounded-md py-4">
-          <div className="flex animate-scroll">
+          <div className="flex w-[200%] animate-scroll">
             {icons.map((icon, index) => (
               <div key={index} className="flex-none mx-4">
                 {icon.icon}
