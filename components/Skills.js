ee0d2c7226e7b6a4957317a25c64838d254ed6ee
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 20:07:42 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaPython, FaJs, FaReact } from 'react-icons/fa';
-import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
+import { SiTailwindcss, SiNextjs } from 'react-icons/si';
 
 export default function Skills() {
 
