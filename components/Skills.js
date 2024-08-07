392ca433e16e5984bed36076d971833bc4a27673
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 19:05:20 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -10,19 +10,19 @@ export default function Skills() {
   };
 
   return (
-    <div className="py-4 m-4 bg-[#a5a5a5]/60 rounded-lg">
+    <div className="py-3 m-4 bg-[#a5a5a5]/60 rounded-lg">
       <h1 className="text-4xl text-black font-monaSans m-4">Skills</h1>
       <h5 className="text-sm text-left font-monaSansM m-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
-   <div className="mx-2">
-      <div className="py-3 m-4 bg-gradient-to-br from-[#b6b6b6] via-[#71747B] to-[#4A4B4F] rounded-lg">
+   <div className="mx-4">
+      <div className="py-3 m-4 border border-[#b6b6b6] bg-gradient-to-br via-[#71747B] to-[#4A4B4F] rounded-lg">
         <h2 className="text-xl text-gray-700 font-monaSansM m-2">Languages</h2>
         <div className="flex flex-wrap mt-2">
           {languages.map((language, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-1 ml-1 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 text-center text-sm text-white"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[language]}`}></div>
               {language}
@@ -34,7 +34,7 @@ export default function Skills() {
           {frameworks.map((framework, index) => (
             <div
               key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-1 ml-1 text-center text-sm text-white"
+              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 text-center text-sm text-white"
             >
               <div className={`w-3 h-3 rounded-full mr-2 ${circleColors[framework]}`}></div>
               {framework}
