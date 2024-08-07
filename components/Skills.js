2c5f45ba5067c4afceeeb7d2ca3dc660970168c2
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-07 19:25:57 +0530

    Update Skills.js

--- components/Skills.js
+++ components/Skills.js
@@ -8,30 +8,30 @@ export default function Skills() {
       <h5 className="text-sm text-left font-monaSansM m-4">
         Here are some languages and frameworks I've learnt in a year.
       </h5>
-      <div className="py-3 m-3 border border-[#b6b6b6] bg-gradient-to-br from-[#4A4B4F]/50 via-[#71747B] to-[#4A4B4F] opacity-90 rounded-lg">
-        <h2 className="text-xl text-gray-300 font-monaSansM m-2">Languages</h2>
-        <div className="flex flex-wrap mt-2">
-          {languages.map((language, index) => (
-            <div
-              key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-2 text-center text-sm text-white"
-            >
-              {language}
-            </div>
-          ))}
-        </div>
-        <h2 className="text-xl text-gray-300 font-monaSansM m-2">Frameworks</h2>
-        <div className="flex flex-wrap mt-2">
-          {frameworks.map((framework, index) => (
-            <div
-              key={index}
-              className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-gray-700 to-gray-800 p-2 m-2 text-center text-sm text-white"
-            >
-              {framework}
-            </div>
-          ))}
-        </div>
+      <div className="py-4 px-6 m-4 border border-gray-300 bg-gradient-to-br from-blue-900 via-gray-700 to-gray-800 opacity-90 rounded-lg shadow-lg">
+  <h2 className="text-2xl text-white font-semibold m-3">Languages</h2>
+  <div className="flex flex-wrap mt-4">
+    {languages.map((language, index) => (
+      <div
+        key={index}
+        className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-blue-800 to-blue-900 p-3 m-2 text-center text-sm text-white transition-transform transform hover:scale-105"
+      >
+        {language}
       </div>
+    ))}
+  </div>
+  <h2 className="text-2xl text-white font-semibold m-3 mt-6">Frameworks</h2>
+  <div className="flex flex-wrap mt-4">
+    {frameworks.map((framework, index) => (
+      <div
+        key={index}
+        className="flex items-center rounded-md border border-gray-500 bg-gradient-to-b from-blue-800 to-blue-900 p-3 m-2 text-center text-sm text-white transition-transform transform hover:scale-105"
+      >
+        {framework}
+      </div>
+    ))}
+  </div>
+</div>
     </div>
   );
 }
