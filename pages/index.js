9bf45274a10f5d85b5268a5e427cf95a919ea10f
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-08 12:32:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Skills from "../components/Skills";
 export default function HomePage() {
   return (
     <div className="min-h-screen font-mono bg-gradient-to-br from-[#a5a5a5] to-[#d5d5d5] text-[#4B5462]  relative overflow-hidden">
-      <Particles quantity={100} />
+      <Particles quantity={150} />
       <main className="pt-20 relative">
         <Header />
         <Profile />
