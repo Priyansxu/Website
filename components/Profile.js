710cfaa1885b4671db62ce537ae69beb7396b4e3
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-08 10:59:18 +0530

    Update Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -27,7 +27,9 @@ export default function Profile() {
 
   return (
     <section ref={profileRef} className="flex flex-col m-4 lg:mx-8">
-      <h1 className="text-4xl text-black font-monaSans mb-4">About me</h1>
+    <div className="py-3 mx-4 my-2 bg-[#a5a5a5]/60 rounded-md">
+      <h1 className="text-4xl text-black font-monaSans">About me</h1>
+  </div>
       <motion.div
         className="relative w-full rounded-md flex bg-gradient-to-br from-[#b6b6b6] via-[#71747B]/50 to-[#b6b6b6] opacity-30 shadow-xl overflow-hidden backdrop-blur-md z-30"
         initial="hidden"
