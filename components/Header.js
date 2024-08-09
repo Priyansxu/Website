d01959e8d0f215b58d8c5886e6de47f224ad20f7
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-09 23:03:08 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,16 +10,15 @@ export default function Header() {
   return (
     <section className="relative text-center">
       <motion.div
-        className="absolute top-0 left-0 w-full bg-black"
+        className="top-0 left-0 w-full h-16 bg-black"
         initial={{ clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)" }}
         animate={{ clipPath: "polygon(0% 0%, 94% 0%, 100% 20%, 100% 100%, 0% 100%, 0% 20%, 6% 0%)" }}
         transition={{ duration: 1.5, ease: "easeInOut" }}
-        style={{ height: "10vh" }}
       ></motion.div>
-      
-      <div className="relative p-6 z-10">
+
+      <div className="p-6">
         <motion.h1
-          className="text-4xl font-bold text-white bg-clip-text pb-3"
+          className="text-4xl font-bold text-black bg-clip-text pb-3"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
@@ -27,7 +26,7 @@ export default function Header() {
           Priyanshu
         </motion.h1>
         <motion.p
-          className="mt-4 text-lg font-spaceMono text-gray-300 backdrop-blur-md"
+          className="mt-4 text-lg font-spaceMono text-gray-600 backdrop-blur-md"
           initial="hidden"
           animate="visible"
           variants={FadeInStagger}
