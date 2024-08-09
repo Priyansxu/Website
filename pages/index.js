9cdbe0f67c311d858fe101180f31b38cdebe5d5a
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-09 22:38:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,12 @@ import Particles from "../components/Particles";
 import Header from "../components/Header";
 import Profile from "../components/Profile";
 import Skills from "../components/Skills";
-import Shape from "../components/Shape";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen font-mono bg-gradient-to-br from-[#a5a5a5] to-[#d5d5d5] text-[#4B5462] relative overflow-hidden">
       <Particles quantity={150} />
       <main className="pt-20 relative">
-        <Shape className="z-30" />
         <Header />
         <Profile />
         <Skills />
