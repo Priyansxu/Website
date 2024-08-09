156be0bbddf0204b992ca3f36ce5d5cb4cc5715e
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-09 22:30:24 +0530

    Update Shape.js

--- components/Shape.js
+++ components/Shape.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function Shape() {
   return (
     <motion.div
-      className="absolute top-0 left-0 w-full bg-black"
+      className="absolute top-0 left-0 w-full bg-black z-30"
       initial={{ clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)" }}
       animate={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 60%, 85% 100%, 15% 100%, 0% 60%)" }}
       transition={{ duration: 2, ease: "easeInOut" }}
