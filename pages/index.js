54e1b5ec022ae98a443aeb40396627f2e516a8f1
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-09 22:45:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen font-mono bg-gradient-to-br from-[#a5a5a5] to-[#d5d5d5] text-[#4B5462] relative overflow-hidden">
       <Particles quantity={150} />
-      <main className="pt-20 relative">
+      <main className="relative">
         <Header />
         <Profile />
         <Skills />
