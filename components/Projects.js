1dfa3dfee6c2111ebfca8ece4ea42dcaf7646ac8
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-10 22:17:10 +0530

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -22,28 +22,28 @@ export default function Projects() {
   ];
 
   return (
-    <div ref={projectsRef} className="py-3 my-8 lg:my-10 m-4 bg-[#a5a5a5]/60 rounded-md shadow-xl">
+    <motion.div
+      ref={projectsRef}
+      className="py-3 my-8 lg:my-10 m-4 bg-[#a5a5a5]/60 rounded-md shadow-xl"
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={FadeInStagger}
+    >
       <h1 className="text-4xl text-black font-monaSans m-4">Projects</h1>
       <h5 className="text-sm md:text-xl lg:text-2xl text-left font-monaSansM m-4">
         Here are some projects I've worked on recently.
       </h5>
-      <div className="m-3">
-        <motion.div
-          className="relative overflow-hidden bg-[#b6b6b6] rounded-md backdrop-blur-md z-30 py-4"
-          initial="hidden"
-          animate={isInView ? "visible" : "hidden"}
-          variants={FadeInStagger}
-        >
-          <div className="flex flex-col space-y-4">
-            {projects.map((project, index) => (
-              <div key={index} className="flex-none mx-4 p-4 bg-white rounded-md shadow-md">
-                <h3 className="text-xl font-bold mb-2">{project.name}</h3>
-                <p className="text-md">{project.description}</p>
-              </div>
-            ))}
+      <div className="flex flex-col space-y-4 m-3">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex-none mx-4 p-4 rounded-md shadow-md bg-[#8a8a8a] text-white"
+          >
+            <h3 className="text-xl font-bold mb-2">{project.name}</h3>
+            <p className="text-md">{project.description}</p>
           </div>
-        </motion.div>
+        ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
