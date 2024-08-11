22d6314b03310cf3114812037553b0b4ffeca555
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-11 11:59:27 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
         className="bg-[#686868] -mt-4 md:-mt-16 mb-4 sm:h-[3vh] md:h-[2vh] lg:h-[1vh] mx-0 md:mx-10"
         initial={{ 
           clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)",
-          filter: "blur(40px)",
+          filter: "blur(10px)",
           opacity: 0,
         }}
         animate={{ 
@@ -24,7 +24,7 @@ export default function Header() {
         transition={{ duration: 0.5, ease: "easeInOut" }}
         style={{ height: "5vh" }}
       ></motion.div>
-<h1 className="font-celtG text-9xl md:text-15xl lg:text-20xl align-center text-[#D4D5D9] -mx-6 md:-mx-9 w-full mt-8 py-6 rotate-180">Priyanshu</h1>
+<h1 className="font-celtG text-9xl md:text-15xl lg:text-20xl align-center text-[#D4D5D9] -mx-4 md:-mx-9 w-full mt-8 py-6 rotate-180">Vengeance</h1>
       <div className="pb-20 px-8">
         <motion.h1
           className="font-celtG text-4xl tracking-wide text-[#686868] bg-clip-text pb-3"
@@ -52,7 +52,7 @@ export default function Header() {
             alt="Fluid"
             height={150}
             width={600}
-            className="relative opacity-70 w-full max-w-md"
+            className="relative opacity-60 w-full max-w-md"
           />
         </div>
       </div>
