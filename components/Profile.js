f21ac0a2ecf7cb975442b1d39d95b312bb94cfdd
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-11 12:33:54 +0530

    Update Profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -27,7 +27,7 @@ export default function Profile() {
 
   return (
     <section ref={profileRef} className="flex flex-col m-4 lg:mx-8 bg-gray border border-[#D3D4D6] shadow-inner rounded-md">
-      <div className="p-4 -mb-2 bg-gradient-to-b from-gray to-gravi rounded-md">
+      <div className="p-4 -mb-2 bg-gradient-to-b from-[#DEDFE3] to-[#DBDCDE] rounded-md">
         <h1 className="text-4xl text-black/60 font-monaSans mt-2">About me</h1>
       </div>
       <motion.div
