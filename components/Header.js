fd7527777872f7e0c5449c1c2eae2a1d9471abe4
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-11 11:55:06 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ export default function Header() {
         transition={{ duration: 0.5, ease: "easeInOut" }}
         style={{ height: "5vh" }}
       ></motion.div>
-<h1 className="font-celtG text-9xl md:text-15xl lg:text-20xl align-center text-[#D4D5D9] -mx-4 md:-mx-9 w-full mt-8 py-6 rotate-180"> Priyanshu</h1>
+<h1 className="font-celtG text-9xl md:text-15xl lg:text-20xl align-center text-[#D4D5D9] -mx-4 md:-mx-9 -mr-6 w-full mt-8 py-6 rotate-180">Priyanshu</h1>
       <div className="pb-20 px-8">
         <motion.h1
           className="font-celtG text-4xl tracking-wide text-[#686868] bg-clip-text pb-3"
